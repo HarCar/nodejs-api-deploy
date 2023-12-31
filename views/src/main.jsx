89112087b9976a60547ca1dc3087bf08b15149d9
Harvy <harvycardoza@gmail.com>
2023-12-31 10:09:05 -0600

    mejoras

--- views/src/main.jsx
+++ views/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ComboBox from './Video.jsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ComboBox />
+    <App />
   </React.StrictMode>,
 )
